a72a68b84e22dfd79d8b6928172b8e332fd0b3f2
D13gx1 <136347264+D13gx1@users.noreply.github.com>
2024-11-03 22:44:32 -0300

    FINAL
    asa

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -57,143 +57,15 @@ export class WelcomeComponent implements OnInit {
     this.router.navigate(['/login']); // Redirige al usuario a la página de login
   }
   
-  async comenzarEscaneoQR() {
-    try {
-      const mediaProvider = await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: 'environment' }
-      });
-      this.video.nativeElement.srcObject = mediaProvider;
-      this.video.nativeElement.setAttribute('playsinline', 'true');
-      this.loading = await this.loadingController.create({});
-      await this.loading.present();
-      this.video.nativeElement.play();
-      requestAnimationFrame(this.verificarVideo.bind(this));
-    } catch (err) {
-      console.error('Error al inicializar la cámara: ', err);
-      const alert = await this.alertController.create({
-        header: 'Error',
-        message: 'No se pudo iniciar la cámara. Por favor, revisa los permisos o el estado de tu dispositivo.',
-        buttons: ['OK']
-      });
-      await alert.present();
-    }
-  }
 
   // Verifica continuamente el video hasta que detecte un código QR
-  async verificarVideo() {
-    if (this.video.nativeElement.readyState === this.video.nativeElement.HAVE_ENOUGH_DATA) {
-      if (this.loading) {
-        await this.loading.dismiss();
-        this.loading = null;
-        this.escaneando = true;
-      }
 
-      if (this.obtenerDatosQr()) {
-        console.log('datos obtenidos');
-      } else {
-        if (this.escaneando) {
-          console.log('escaneando...');
-          requestAnimationFrame(this.verificarVideo.bind(this));
-        }
-      }
-    } else {
-      console.log('video aún no tiene datos');
-      requestAnimationFrame(this.verificarVideo.bind(this));
-    }
-  }
 
   // Extrae los datos del código QR usando jsQR
-  public obtenerDatosQr(source?: CanvasImageSource): boolean {
-    let w = 0;
-    let h = 0;
 
-    if (!source) {
-      this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
-      this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
-    }
-
-    w = this.canvas.nativeElement.width;
-    h = this.canvas.nativeElement.height;
-
-    const context: CanvasRenderingContext2D = this.canvas.nativeElement.getContext('2d');
-    context.drawImage(source ? source : this.video.nativeElement, 0, 0, w, h);
-    const img: ImageData = context.getImageData(0, 0, w, h);
-    const qrCode = jsQR(img.data, img.width, img.height, { inversionAttempts: 'dontInvert' });
-
-    if (qrCode) {
-      this.escaneando = false;
-      this.datosQR = qrCode.data;
-      this.mostrarDatosQROrdenados(this.datosQR);
-    }
-
-    return this.datosQR !== '';
-  }
 
   // Ordena los datos del QR y los redirige a la página Mi Curso
-  public mostrarDatosQROrdenados(datosQR: string): void {
-    try {
-      const objetoDatosQR = JSON.parse(datosQR);
-      this.sede = objetoDatosQR.sede;
-      this.idAsignatura = objetoDatosQR.idAsignatura;
-      this.seccion = objetoDatosQR.seccion;
-      this.nombreAsignatura = objetoDatosQR.nombreAsignatura;
-      this.nombreProfesor = objetoDatosQR.nombreProfesor;
-      this.dia = objetoDatosQR.dia;
-      this.horaInicio = objetoDatosQR.horaInicio;
-      this.horaFin = objetoDatosQR.horaFin;
-      this.bloqueInicio = objetoDatosQR.bloqueInicio;
-      this.bloqueTermino = objetoDatosQR.bloqueTermino;
 
-      // Redirige a la página Mi Curso pasando los datos
-      this.router.navigate(['/tabs/mi-curso'], {
-        state: {
-          sede: this.sede,
-          idAsignatura: this.idAsignatura,
-          seccion: this.seccion,
-          nombreAsignatura: this.nombreAsignatura,
-          nombreProfesor: this.nombreProfesor,
-          dia: this.dia,
-          horaInicio: this.horaInicio,
-          horaFin: this.horaFin,
-          bloqueInicio: this.bloqueInicio,
-          bloqueTermino: this.bloqueTermino,
-        }
-      });
-    } catch (error) {
-      console.error('Error al procesar el código QR', error);
-    }
-  }
-
-  // Permite cargar una imagen desde un archivo para escanear el código QR
-  public cargarImagenDesdeArchivo(): void {
-    this.fileinput.nativeElement.click();
-  }
-
-  // Verifica el archivo cargado y escanea el QR si existe
-  public verificarArchivoConQr(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    if (input && input.files && input.files.length > 0) {
-      const file = input.files[0];
-      const img = new Image();
-      img.onload = () => {
-        this.obtenerDatosQr(img);
-      };
-      img.src = URL.createObjectURL(file);
-    }
-  }
-
-  // Detiene el escaneo de QR
-  public detenerEscaneoQR(): void {
-    this.escaneando = false;
-  }
-
-  // Limpia los datos y reinicia los valores
-  public limpiarDatos(): void {
-    this.escaneando = false;
-    this.datosQR = '';
-    this.loading = null;
-    (document.getElementById('input-file') as HTMLInputElement).value = '';
-  }
 
   // Muestra una alerta de confirmación para cerrar sesión
   async confirmLogout() {
