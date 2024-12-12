8aead6288ed0d99e85e4ea5ea361e97a4e31d667
EvalunaGM <134944996+EvalunaGM@users.noreply.github.com>
2024-12-11 21:54:47 -0300

    asa
    asa

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 rem ionic cap run android -l --external --host=192.168.100.37 --port=8100 --target=R58T614VCGP
-ionic cap run android -l --external --host=192.168.1.2 --port=8101 --target=R5CT425D3JE
+ionic cap run android -l --external --host=192.168.157.114 --port=8101 --target=R5CT425D3JE
